532ce346e0580ea398483731ba7b4c943977f1aa
“ijasz” <jass07rtr@gmail.com>
2023-01-12 13:46:19 +0530

    added files at 12/01/23

---
+++ JavaScript/11_01_23/index.js
@@ -0,0 +1,68 @@
+// Array☺
+
+// const a = [1, 2, 3, 4, 5];
+// const b = [6, 7, 8, 9, 10];
+
+// Spread operator
+// const c = [...a, ...b, 11, 12];
+
+// Rest operator
+
+// const [one, two, three, ...rest] = c;
+
+// console.log(one);
+// console.log(two);
+// console.log(three);
+// console.log(rest);
+
+// Object☺
+
+// const a = {
+//   firstName: "Arthi",
+//   lastname: "Harikrishnan",
+//   age: 10,
+// };
+
+// const b = {
+//   skills: ["HTML", "CSS", "JS"],
+//   location: "pondy",
+// };
+
+// // Spread operator
+// const c = { ...a, ...b };
+
+// // Rest operator
+// const { skills, firstName, lastname, ...rest } = c;
+
+// console.log(c);
+// console.log(skills);
+// console.log(firstName + " " + lastname);
+// console.log(`${firstName} ${lastname}`);
+// console.log(rest);
+// console.log("hfdgj\nkd");
+// console.log(`i am good human being`);
+
+// String Method
+
+// const a = "Arthi";
+
+// console.log(a.length);
+// console.log(a.toUpperCase());
+// console.log(a.toLocaleLowerCase());
+// console.log(a.length);
+
+// Mutable
+
+const a = [1, 2, 4];
+
+a[2] = 3;
+
+console.log(a);
+
+// Immutable
+
+let b = "Arthi";
+
+b[0] = "a";
+
+console.log(b[0]);
---
+++ JavaScript/12_01_23/index.js
@@ -0,0 +1,11 @@
+// IF condition
+
+// const input = parseInt(prompt("Enter a number"));
+
+// if (input < 0) {
+//   console.log("It is Negative Number☺");
+//   console.log(input, typeof input);
+// } else {
+//   console.log("It is Positive Number☺");
+//   console.log(input, typeof input);
+// }
