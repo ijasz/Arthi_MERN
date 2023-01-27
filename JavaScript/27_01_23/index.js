fe4c79295a47f9a3c69aeb946299f811fccf2a25
“ijasz” <jass07rtr@gmail.com>
2023-01-27 13:57:05 +0530

    added a file at 27/01/23

--- JavaScript/27_01_23/index.js
+++ JavaScript/27_01_23/index.js
@@ -1,14 +1,30 @@
 const doc = window.document;
 
-// console.log(doc);
+// // console.log(doc);
 
-const text = doc.createElement("h1");
+// const text = doc.createElement("h1");
 
-text.style.color = "red";
-text.style.fontSize = "50px";
+// text.style.color = "red";
+// text.style.fontSize = "50px";
 
-text.innerText = "hello";
+// text.innerText = "hello";
 
-doc.body.appendChild(text);
+// doc.body.appendChild(text);
 
-// console.log(h1);
+// // console.log(h1);
+
+for (let i = 128000; i <= 128000 + 400; i++) {
+  const h1 = doc.createElement("h1");
+  if (i % 2 == 0) {
+    h1.style.color = "green";
+  } else {
+    h1.style.color = "red";
+  }
+  h1.innerHTML = `${i} &#${i};`;
+  console.log(h1);
+  doc.body.appendChild(h1);
+}
+
+// const name = "arthi";
+
+// console.log(`My name is ${name}`);
