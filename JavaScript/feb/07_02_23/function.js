62d3b51183cf2145e5c3af4a280e6f6c7c2183f2
“ijasz” <jass07rtr@gmail.com>
2023-02-07 14:06:25 +0530

    added files at 07/02/23

---
+++ JavaScript/feb/07_02_23/function.js
@@ -0,0 +1,39 @@
+// Function Declaration / Function Statement
+// function username(name = "") {
+//   return `Myself ${name}`;
+// }
+
+// // console.log(username("arthi"));
+// // console.log(username("ashwathi"));
+// // console.log(username());
+
+// let a = username("arthi");
+// let b = username("ashwathi");
+
+// console.log(a);
+// console.log(b);
+
+// Global Execution Context & Local Execution Context
+
+var a = 10;
+
+function sum(a) {
+  var b = 20;
+  console.log(a + b);
+}
+
+sum(10);
+
+let count = 0;
+
+function onPress(a) {
+  count++;
+  if (count === 1) {
+    a.style.color = "red";
+    a.innerHTML = "you clicked me";
+  } else if (count === 2) {
+    a.style.color = "blue";
+    a.innerHTML = "you clicked me";
+  }
+  console.log("you clicked me", a);
+}
